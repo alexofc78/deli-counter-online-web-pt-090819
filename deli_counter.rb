de984caf11da10c2d531982d76b1d554536faff6
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-18 17:06:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,12 @@
 def line(array)
-  final_phrase = ""
   if array.length == 0
   puts "The line is currently empty."
   elsif
   phrase = "The line is currently:"
   array.each_with_index do |name, index|
-    final_phrase = phrase + " #{index + 1}. #{name}."
+    phrase = phrase + " #{index + 1}. #{name}."
   end
-  puts final_phrase
+  puts phrase
 end
 end
 
