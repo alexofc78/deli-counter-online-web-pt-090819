b8ce4f512aa1a71dd7e34befb25aa56c3dd7d26e
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-18 16:41:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,8 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  "Welcome, " name ". You are number " katz_deli.length " in line."
+  #katz_deli.push(name)
+  #"Welcome, " name ". You are number " katz_deli.length " in line."
 end
 
 def now_serving(katz_deli)
